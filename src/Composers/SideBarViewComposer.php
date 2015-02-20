461501e53b7b5a66d6a30495921ee99fc167e39d
Samuel De Backer <sdebacker@gmail.com>
2015-02-20 01:32:28 +0100

    licence + sidebar

--- src/Composers/SideBarViewComposer.php
+++ src/Composers/SideBarViewComposer.php
@@ -1,18 +1,25 @@
 <?php
 namespace TypiCMS\Modules\Projects\Composers;
 
-use Illuminate\View\View;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Sidebar\SidebarGroup;
+use Maatwebsite\Sidebar\SidebarItem;
+use TypiCMS\Composers\BaseSidebarViewComposer;
 
-class SidebarViewComposer
+class SidebarViewComposer extends BaseSidebarViewComposer
 {
     public function compose(View $view)
     {
-        $view->menus['content']->put('projects', [
-            'weight' => config('typicms.projects.sidebar.weight'),
-            'request' => $view->prefix . '/projects*',
-            'route' => 'admin.projects.index',
-            'icon-class' => 'icon fa fa-fw fa-cube',
-            'title' => 'Projects',
-        ]);
+        $view->sidebar->group(trans('global.menus.content'), function (SidebarGroup $group) {
+            $group->addItem(trans('projects::global.name'), function (SidebarItem $item) {
+                $item->icon = config('typicms.projects.sidebar.icon', 'icon fa fa-fw fa-cube');
+                $item->weight = config('typicms.projects.sidebar.weight');
+                $item->route('admin.projects.index');
+                $item->append('admin.projects.create');
+                $item->authorize(
+                    $this->auth->hasAccess('projects.index')
+                );
+            });
+        });
     }
 }
