d1dbecce8029a8190725d2a9f2b1f38f6506a8f0
Samuel De Backer <sdebacker@gmail.com>
2015-03-18 23:12:12 +0100

    _index.blade

--- src/resources/views/admin/_index.blade.php
+++ src/resources/views/admin/_index.blade.php
@@ -1,8 +1,8 @@
 <div ng-app="typicms" ng-cloak ng-controller="ListController">
 
     <h1>
-        <a href="{{ url }}/create" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only" translate>New</span></a>
-        <span translate translate-n="models.length" translate-plural="{{ models.length }} projects">{{ models.length }} project</span>
+        <a href="{{ route('admin.' . $module . '.create') }}" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only" translate>New</span></a>
+        <span translate translate-n="models.length" translate-plural="@{{ models.length }} projects">@{{ models.length }} project</span>
     </h1>
 
     <div class="btn-toolbar" role="toolbar" ng-include="'/views/partials/btnLocales.html'"></div>
@@ -22,10 +22,10 @@
                 <tr>
                     <td colspan="4"></td>
                     <td>
-                        <input st-search="'title'" class="form-control input-sm" placeholder="{{ 'Search' | translate }}…" type="text">
+                        <input st-search="'title'" class="form-control input-sm" placeholder="@{{ 'Search' | translate }}…" type="text">
                     </td>
                     <td>
-                        <input st-search="'category_name'" class="form-control input-sm" placeholder="{{ 'Search' | translate }}…" type="text">
+                        <input st-search="'category_name'" class="form-control input-sm" placeholder="@{{ 'Search' | translate }}…" type="text">
                     </td>
                 </tr>
             </thead>
@@ -35,9 +35,11 @@
                     <td><typi-btn-delete ng-click="delete(model)"></typi-btn-delete></td>
                     <td typi-btn-edit></td>
                     <td typi-btn-status></td>
-                    <td typi-thumb-list-item></td>
-                    <td>{{ model.title }}</td>
-                    <td>{{ model.category_name }}</td>
+                    <td>
+                        <img ng-src="@{{ model.thumb }}" alt="">
+                    </td>
+                    <td>@{{ model.title }}</td>
+                    <td>@{{ model.category_name }}</td>
                 </tr>
             </tbody>
             <tfoot>
