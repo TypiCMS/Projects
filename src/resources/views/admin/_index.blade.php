b3ee6fdd3555c6048d19ce4cee2ad92def54845d
Samuel De Backer <sdebacker@gmail.com>
2015-05-22 22:31:05 +0200

    angular gettext removed

--- src/resources/views/admin/_index.blade.php
+++ src/resources/views/admin/_index.blade.php
@@ -1,8 +1,8 @@
 <div ng-app="typicms" ng-cloak ng-controller="ListController">
 
     <h1>
-        <a href="{{ route('admin.' . $module . '.create') }}" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only" translate>New</span></a>
-        <span translate translate-n="models.length" translate-plural="@{{ models.length }} projects">@{{ models.length }} project</span>
+        <a href="{{ route('admin.' . $module . '.create') }}" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only">New</span></a>
+        <span>@{{ models.length }} @choice('projects::global.projects', 2)</span>
     </h1>
 
     <div class="btn-toolbar" role="toolbar" ng-include="'/views/partials/btnLocales.html'"></div>
@@ -14,18 +14,18 @@
                 <tr>
                     <th class="delete"></th>
                     <th class="edit"></th>
-                    <th st-sort="status" class="status st-sort" translate>Status</th>
-                    <th st-sort="image" class="image st-sort" translate>Image</th>
-                    <th st-sort="title" class="title st-sort" translate>Title</th>
-                    <th st-sort="category_name" class="category st-sort" translate>Category</th>
+                    <th st-sort="status" class="status st-sort">Status</th>
+                    <th st-sort="image" class="image st-sort">Image</th>
+                    <th st-sort="title" class="title st-sort">Title</th>
+                    <th st-sort="category_name" class="category st-sort">Category</th>
                 </tr>
                 <tr>
                     <td colspan="4"></td>
                     <td>
-                        <input st-search="title" class="form-control input-sm" placeholder="@{{ 'Search' | translate }}…" type="text">
+                        <input st-search="title" class="form-control input-sm" placeholder="@lang('global.Search')…" type="text">
                     </td>
                     <td>
-                        <input st-search="category_name" class="form-control input-sm" placeholder="@{{ 'Search' | translate }}…" type="text">
+                        <input st-search="category_name" class="form-control input-sm" placeholder="@lang('global.Search')…" type="text">
                     </td>
                 </tr>
             </thead>
