5f03478ac559ba84971fcd0f0c9d04ef38dedfa6
Samuel De Backer <sdebacker@gmail.com>
2015-12-28 21:43:41 +0100

    retain old input

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -12,10 +12,10 @@
 @include('core::admin._galleries-fieldset')
 
 {!! BootForm::select(trans('validation.attributes.category_id'), 'category_id', Categories::allForSelect()) !!}
-{!! BootForm::text(trans('validation.attributes.tags'), 'tags')->value(implode(', ', $model->tags->pluck('tag')->all())) !!}
+{!! BootForm::text(trans('validation.attributes.tags'), 'tags')->value(old('tags') ? : implode(', ', $model->tags->pluck('tag')->all())) !!}
 <div class="row">
     <div class="col-sm-6">
-        {!! BootForm::date(trans('validation.attributes.date'), 'date')->value($model->present()->dateOrNow('date'))->addClass('datepicker') !!}
+        {!! BootForm::date(trans('validation.attributes.date'), 'date')->value(old('date') ? : $model->present()->dateOrNow('date'))->addClass('datepicker') !!}
     </div>
 </div>
 {!! BootForm::text(trans('validation.attributes.website'), 'website')->placeholder('http://') !!}
