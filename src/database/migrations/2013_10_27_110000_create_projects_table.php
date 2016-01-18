f63703d33ffdee512666a5643e5024da4dcdbf1f
Samuel De Backer <sdebacker@gmail.com>
2016-01-18 21:47:27 +0100

    slug unique

--- src/database/migrations/2013_10_27_110000_create_projects_table.php
+++ src/database/migrations/2013_10_27_110000_create_projects_table.php
@@ -14,39 +14,29 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->engine = 'InnoDB';
-
             $table->increments('id');
             $table->integer('category_id')->unsigned()->nullable();
             $table->date('date');
             $table->string('website')->nullable();
             $table->string('image')->nullable();
-
             $table->timestamps();
-
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
 
         Schema::create('project_translations', function (Blueprint $table) {
             $table->engine = 'InnoDB';
-
             $table->increments('id');
             $table->integer('project_id')->unsigned();
-
             $table->string('locale')->index();
-
             $table->boolean('status')->default(0);
-
             $table->string('title');
             $table->string('slug')->nullable();
-
             $table->text('summary');
             $table->text('body');
-
             $table->timestamps();
-
             $table->unique(['project_id', 'locale']);
+            $table->unique(['locale', 'slug']);
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
-
         });
     }
 
