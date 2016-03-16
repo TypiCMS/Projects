6fab4e287ea66f5515851675697de1b3b80f63a5
Rafael Perelló Rowley <rperellorow@gmail.com>
2016-03-16 12:32:44 +0100

    Added translation to spanish

---
+++ src/resources/lang/es/global.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'name'     => 'Proyectos',
+    'projects' => 'proyecto|proyectos',
+    'New'      => 'Nuevo proyecto',
+    'Edit'     => 'Editar proyecto',
+    'Back'     => 'Volver a los proyectos',
+];
