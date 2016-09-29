3866a82e7473997277d9bb2d82e72afa4983f573
Samuel De Backer <sdebacker@gmail.com>
2016-09-29 15:34:21 +0000

    Applied fixes from StyleCI

--- src/Providers/ModuleProvider.php
+++ src/Providers/ModuleProvider.php
@@ -3,7 +3,6 @@
 namespace TypiCMS\Modules\Projects\Providers;
 
 use Illuminate\Foundation\AliasLoader;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 use TypiCMS\Modules\Core\Facades\TypiCMS;
 use TypiCMS\Modules\Core\Observers\FileObserver;
