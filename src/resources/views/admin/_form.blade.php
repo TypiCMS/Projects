a96437473fefdf09babb103e69fd124d5fa95e5d
Samuel De Backer <sdebacker@gmail.com>
2017-01-20 18:22:49 +0100

    validations attributes in a json file

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -10,18 +10,18 @@
 
 @include('core::form._title-and-slug')
 {!! TranslatableBootForm::hidden('status')->value(0) !!}
-{!! TranslatableBootForm::checkbox(__('validation.attributes.online'), 'status') !!}
+{!! TranslatableBootForm::checkbox(__('Online'), 'status') !!}
 
 @include('core::admin._galleries-fieldset')
 
-{!! BootForm::select(__('validation.attributes.category_id'), 'category_id', Categories::allForSelect()) !!}
-{!! BootForm::text(__('validation.attributes.tags'), 'tags')->value(old('tags') ? : implode(', ', $model->tags->pluck('tag')->all())) !!}
+{!! BootForm::select(__('Category'), 'category_id', Categories::allForSelect()) !!}
+{!! BootForm::text(__('Tags'), 'tags')->value(old('tags') ? : implode(', ', $model->tags->pluck('tag')->all())) !!}
 <div class="row">
     <div class="col-sm-6">
-        {!! BootForm::date(__('validation.attributes.date'), 'date')->value(old('date') ? : $model->present()->dateOrNow('date'))->addClass('datepicker') !!}
+        {!! BootForm::date(__('Date'), 'date')->value(old('date') ? : $model->present()->dateOrNow('date'))->addClass('datepicker') !!}
     </div>
 </div>
-{!! BootForm::text(__('validation.attributes.website'), 'website')->placeholder('http://') !!}
+{!! BootForm::text(__('Website'), 'website')->placeholder('http://') !!}
 
-{!! TranslatableBootForm::textarea(__('validation.attributes.summary'), 'summary')->rows(4) !!}
-{!! TranslatableBootForm::textarea(__('validation.attributes.body'), 'body')->addClass('ckeditor') !!}
+{!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
+{!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor') !!}
