ed4ed0ba05168d07efd1894d7e5af19d0fd8cc2a
Samuel De Backer <sdebacker@gmail.com>
2020-02-12 17:02:48 +0100

    Update _list-item.blade.php

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -3,7 +3,9 @@
         <img class="project-list-item-image" src="{!! $project->present()->image(540, 400) !!}" alt="">
         <div class="project-list-item-info">
             <div class="project-list-item-title">{{ $project->title }}</div>
+            @isset($project->summary)
             <div class="project-list-item-summary">{{ $project->summary }}</div>
+            @endisset
             <div class="project-list-item-date">{{ $project->present()->dateLocalized }}</div>
         </div>
     </a>
