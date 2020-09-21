3d1029680df9eb5c7dc99172bbbb09cc1fb3d78a
Samuel De Backer <sdebacker@gmail.com>
2020-09-21 22:39:53 +0200

    Create _list-results.blade.php

---
+++ src/resources/views/public/_list-results.blade.php
@@ -0,0 +1,10 @@
+<ul class="project-list-results-list">
+    @foreach ($items as $project)
+    <li class="project-list-results-item">
+        <a class="project-list-results-item-link" href="{{ $project->uri() }}">
+            <span class="project-list-results-item-title">{{ $project->title }}</span>
+            <span class="project-list-results-item-date">{{ $project->present()->dateLocalized }}</span>
+        </a>
+    </li>
+    @endforeach
+</ul>
