351b9d7867af5ad0ca71998756c18d5f4c89026f
Samuel De Backer <sdebacker@gmail.com>
2020-12-21 11:10:35 +0100

    Check empty image + missing alt attribute.

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -1,6 +1,8 @@
 <li class="project-list-item">
     <a class="project-list-item-link" href="{{ $project->uri() }}">
-        <img class="project-list-item-image" src="{{ $project->present()->image(540, 400) }}" width="{{ $project->image->width }}" height="{{ $project->image->height }}" alt="">
+        @empty (!$project->image)
+        <img class="project-list-item-image" src="{{ $project->present()->image(540, 400) }}" width="{{ $project->image->width }}" height="{{ $project->image->height }}" alt="{{ $project->image->alt_attribute }}">
+        @endempty
         <div class="project-list-item-info">
             <div class="project-list-item-title">{{ $project->title }}</div>
             @empty(!$project->summary)
--- src/resources/views/public/index.blade.php
+++ src/resources/views/public/index.blade.php
@@ -20,9 +20,9 @@
                 <li class="category-list-item">
                     <a class="category-list-item-link" href="{{ route($lang.'::projects-category', [$category->slug]) }}">
                         <div class="category-list-item-title">{{ $category->title }}</div>
-                        <div class="category-list-item-image-wrapper">
-                            <img class="category-list-item-image" src="{!! $category->present()->image(270, 270) !!}" alt="">
-                        </div>
+                        @empty(!$category->image)
+                        <img class="category-list-item-image" src="{{ $category->present()->image(540, 540) }}" width="{{ $category->image->width }}" height="{{ $category->image->height }}" alt="{{ $category->image->alt_attribute }}">
+                        @endempty
                     </a>
                 </li>
                 @endforeach
