9b17c864132f4e6a14564efea26575255f26bbe1
Samuel De Backer <sdebacker@gmail.com>
2025-10-16 13:06:11 +0200

    Update _form.blade.php

--- resources/views/admin/_form.blade.php
+++ resources/views/admin/_form.blade.php
@@ -7,32 +7,39 @@
 <div class="content">
     <x-core::form-errors />
 
-    <file-manager></file-manager>
-    <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
-    <file-field type="image" field="og_image_id" :init-file="{{ $model->ogImage ?? 'null' }}" label="Open Graph image"></file-field>
-    <files-field :init-files="{{ $model->files }}"></files-field>
+    <div class="row">
+        <div class="col-lg-8">
+            <x-core::title-and-slug-fields :locales="locales()" />
+            <div class="mb-3">
+                {!! TranslatableBootForm::hidden('status')->value(0) !!}
+                {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+            </div>
 
-    <x-core::title-and-slug-fields :locales="locales()" />
-    <div class="mb-3">
-        {!! TranslatableBootForm::hidden('status')->value(0) !!}
-        {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
-    </div>
+            {!! BootForm::select(__('Category'), 'category_id', ProjectCategories::allForSelect())->required() !!}
 
-    {!! BootForm::select(__('Category'), 'category_id', ProjectCategories::allForSelect())->required() !!}
+            {!! BootForm::text(__('Tags'), 'tags')->value(old('tags') ?: $model->tags->pluck('tag')->implode(',')) !!}
 
-    {!! BootForm::text(__('Tags'), 'tags')->value(old('tags') ?: $model->tags->pluck('tag')->implode(',')) !!}
+            <div class="row gx-3 mb-4">
+                @include('taxonomies::admin._checkboxes', ['module' => 'projects'])
+            </div>
 
-    <div class="row gx-3 mb-4">
-        @include('taxonomies::admin._checkboxes', ['module' => 'projects'])
-    </div>
+            <div class="row gx-3">
+                <div class="col-sm-6">
+                    {!! BootForm::date(__('Date'), 'date')->value(old('date') ?: $model->present()->dateOrNow('date'))->addClass('datepicker') !!}
+                </div>
+            </div>
+            {!! BootForm::text(__('Website'), 'website')->type('url')->placeholder('https://') !!}
 
-    <div class="row gx-3">
-        <div class="col-sm-6">
-            {!! BootForm::date(__('Date'), 'date')->value(old('date') ?: $model->present()->dateOrNow('date'))->addClass('datepicker') !!}
+            {!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
+            <x-core::tiptap-editors :model="$model" name="body" :label="__('Body')" />
+        </div>
+        <div class="col-lg-4">
+            <div class="right-column">
+                <file-manager></file-manager>
+                <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
+                <file-field type="image" field="og_image_id" :init-file="{{ $model->ogImage ?? 'null' }}" label="Open Graph image"></file-field>
+                <files-field :init-files="{{ $model->files }}"></files-field>
+            </div>
         </div>
     </div>
-    {!! BootForm::text(__('Website'), 'website')->type('url')->placeholder('https://') !!}
-
-    {!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
-    <x-core::tiptap-editors :model="$model" name="body" :label="__('Body')" />
 </div>
