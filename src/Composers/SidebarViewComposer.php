ec2d0b3eaa010b53e7f51b29ee51087d793e652d
Samuel De Backer <sdebacker@gmail.com>
2025-11-24 11:12:18 +0100

    Make sidebar labels and groups configurable

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -14,10 +14,10 @@ class SidebarViewComposer
         if (Gate::denies('read projects')) {
             return;
         }
-        $view->offsetGet('sidebar')->group(__('Content'), function (SidebarGroup $group): void {
+        $view->offsetGet('sidebar')->group(__(config('typicms.modules.projects.sidebar.group', 'Content')), function (SidebarGroup $group): void {
             $group->id = 'content';
             $group->weight = 30;
-            $group->addItem(__('Projects'), function (SidebarItem $item): void {
+            $group->addItem(__(config('typicms.modules.projects.sidebar.label', 'Projects')), function (SidebarItem $item): void {
                 $item->id = 'projects';
                 $item->icon = config('typicms.modules.projects.sidebar.icon');
                 $item->weight = config('typicms.modules.projects.sidebar.weight');
